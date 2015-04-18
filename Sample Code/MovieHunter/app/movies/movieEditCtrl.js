9bcc2354b85d4c3ee78e786dfae7000586175b15
DeborahK <deborahk@insteptech.com>
2015-04-18 09:47:37 -0700

    First check in of sample code
    Demonstrates routing, data access, forms and validation.

---
+++ Sample Code/MovieHunter/app/movies/movieEditCtrl.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("movieHunter")
+        .controller("MovieEditCtrl",
+                    ["movieResource",
+                        "$routeParams",
+                        "$location",
+                     MovieEditCtrl]);
+
+    function MovieEditCtrl(movieResource, $routeParams, $location) {
+        var vm = this;
+
+        vm.Id = $routeParams.movieId;
+        vm.movie = "";
+        vm.title = "";
+        vm.message = "";
+
+        movieResource.get({ id: vm.Id },
+            function (data) {
+                vm.movie = data;
+
+                if (vm.movieId) {
+                    vm.title = vm.movie.title
+                }
+                else {
+                    vm.title = "New Movie";
+                }
+            });
+
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.movie.$save(function (data) {
+                    vm.message = "Save successful.";
+                })
+            } else {
+                vm.message = "Please correct the validation errors first.";
+            }
+        };
+
+        vm.cancel = function () {
+            $location.path("/searchByTitle")
+        };
+    }
+}());
